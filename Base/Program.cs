4ea39a022cb0a5a52e3e02ce964f950a62caca47
Rocky_Liang <2683737168@qq.com>
2021-04-26 15:57:41 +0800

    添加项目文件。

---
+++ Base/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Dynamic;
+using System.Threading;
+
+
+namespace Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string i = null;
+            DoSomthing(out i);
+            Console.WriteLine(i);
+        }
+        #region out 参数
+        static void DoSomthing(out string parm)
+        {
+            parm="已修改";
+        }
+         
+
+        #endregion
+    }
+}
